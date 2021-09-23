087db2e0f96a5b5eac7dfcef8b4a38a44062c4f2
Mark Worrell <mark@worrell.co.za>
2021-09-23 15:52:30 +0200

    Add sql

---
+++ sql/movie-database.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO `configs` VALUES ('1', 'number_of_pages', '3', '2021-09-23 07:39:50', '2021-09-23 07:39:50');
+INSERT INTO `configs` VALUES ('2', 'number_of_movies', '45', '2021-09-23 07:40:24', '2021-09-23 07:40:24');
+INSERT INTO `configs` VALUES ('3', 'paginate', '9', '2021-09-23 07:46:35', '2021-09-23 07:46:35');
+INSERT INTO `configs` VALUES ('4', 'url', 'https://api.themoviedb.org/3/movie/', '2021-09-23 08:03:05', '2021-09-23 08:03:05');
+INSERT INTO `configs` VALUES ('5', 'apikey', 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMTA2NDhiMDExNjNkNjcyOTc3OGU5ODU3OTAwYzc0YyIsInN1YiI6IjYxNGFjZDMxYzI4MjNhMDA5MGM2NTcwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-j5An9giz1-_NRpmFzYun1P9oGcX2huaN6yZhjCVyeA', '2021-09-23 08:04:05', '2021-09-23 08:04:05');
+INSERT INTO `users` VALUES ('1', 'jointheteam', 'jointheteam@aglet.co.za', null, '$2y$10$6hXFvPnCMjYqH8.mtvOKJeSwfXKxVgYkZtk5Pmk2F.8gOOWDniZSG', null, '2021-09-22 06:58:31', '2021-09-22 06:58:31');
+
